cb432c38b3ba3cef13e9c591bc799e01c826a2cf
sampaiolucasdev <lsampaio32@gmail.com>
2022-03-01 22:19:53 -0300

    Ultimo Modulo

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+
+interface Transaction{
+    id: number,
+    title: string,
+    amount: number,
+    type: string,
+    category: string,
+    createdAt: string,
+
+}
+export function TransactionsTable(){
+
+    const[transactions, setTransactions] = useState<Transaction[]>([]);
+    
+    useEffect(() =>{
+        api.get('transactions') //PEGAR UMA RESPOSTA DA API
+        .then(response =>setTransactions(response.data.transactions)) //E ENTAO ARMAZENAR EM SET TRANSACTIONS
+    }, [])
+
+    return (
+        <Container>
+           <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>                            
+                        <th>Valor</th>                            
+                        <th>Categoria</th>                            
+                        <th>Data</th>                            
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {transactions.map(transaction =>(
+                        <tr key={transaction.id}>                                
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', { //USA O PROPRIO BROWSER COM A FUNÇÃO NATIVA Intl PARA FORMATAÇÃO DE MOEDA
+                                    style: 'currency',
+                                    currency: 'BRL',
+                                }).format(transaction.amount)}
+                            </td>
+                            <td>{transaction.category}</td>
+                            <td>
+                                {new Intl.DateTimeFormat('pt-BR').format(
+                                    new Date(transaction.createdAt)
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table> 
+        </Container>
+
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { createServer, Model } from 'miragejs'
+
+createServer({
+
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+      {
+        id: 1,
+        title: 'Freelance de website',
+        type: 'deposit',
+        amount: 6000,
+        category: 'Dev',
+        createdAt: new Date('2022-03-01'),
+      },
+
+      {
+        id: 2,
+        title: 'Aluguel',
+        type: 'withdraw',
+        amount: 1100,
+        category: 'Casa',
+        createdAt: new Date('2022-03-11'),
+      }
+    
+      ]
+    })
+  },
+ 
+      
+
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions', () =>{
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) =>{
+      const data = JSON.parse(request.requestBody) //OS DADOS DO REQUEST VEM EM string, ENTAO PRECISA CONVERTER PRA JSON PARA UTILIZAR.
+
+      return schema.create('transaction', data);
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
